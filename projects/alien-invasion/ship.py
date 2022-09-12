5d4df968718bb0d6722ebc013dc2857ecfc985e2
Luka Jioshvili <luka.jioshvili20@gmail.com>
2022-09-12 19:55:07 +0400

    added keyboard events

--- projects/alien-invasion/ship.py
+++ projects/alien-invasion/ship.py
@@ -13,6 +13,17 @@ class Ship:
         # Ship location
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # Flag for movement
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        # Update ship view position
+        if self.moving_right:
+            self.rect.x += 1
+        if self.moving_left:
+            self.rect.x -= 1
+
     def blitme(self):
         # Draw ship to current location
         self.screen.blit(self.image, self.rect)
