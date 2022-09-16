3bc79692ebc928c1674743569b0689f694d5dbb4
Luka Jioshvili <luka.jioshvili20@gmail.com>
2022-09-16 20:55:35 +0400

    added fleet movement

--- projects/alien-invasion/alien.py
+++ projects/alien-invasion/alien.py
@@ -7,13 +7,25 @@ class Alien(Sprite):
     def __init__(self, ai_game):
         super().__init__()
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         # Load Alien image and set its rect attribute
         self.image = pygame.image.load('settings/images/alien.bmp')
         self.rect = self.image.get_rect()
 
-        # Alien default positon
+        # Alien default position
         self.rect.x = self.rect.width
         self.rect.y = self.rect.height
 
         # Saving/Storing alien's X position
         self.x = float(self.rect.x)
+
+    # Check if is at the edge of the screen
+    def check_edges(self):
+        screen_rect = self.screen.get_rect()
+        if self.rect.right >= screen_rect.right or self.rect.left <= 0:
+            return True
+
+    def update(self):
+        # Alien movement
+        self.x += (self.settings.alien_speed * self.settings.fleet_direction)
+        self.rect.x = self.x
