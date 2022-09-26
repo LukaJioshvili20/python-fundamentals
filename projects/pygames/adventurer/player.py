6ece68e7896b3f6510a63bcff582b5079366252a
Luka Jioshvili <luka.jioshvili20@gmail.com>
2022-09-26 22:53:02 +0400

    added animation statues

--- projects/pygames/adventurer/player.py
+++ projects/pygames/adventurer/player.py
@@ -17,6 +17,9 @@ class Player(pygame.sprite.Sprite):
         self.gravity = 0.8
         self.jump_speed = -16
 
+        self.status = 'idle'
+        self.facing_right = True
+
     def import_character_assets(self):
         character_path = 'graphics/character/'
         self.animations = {'idle': [], 'run': [], 'jump': [], 'fall': []}
@@ -28,25 +31,44 @@ class Player(pygame.sprite.Sprite):
 
     # Picking animation types
     def animate_character(self):
-        animation = self.animations['idle']
+        animation = self.animations[self.status]
         # Looping trough frames
         self.frame_index += self.animation_speed
         if self.frame_index >= len(animation):
             self.frame_index = 0
 
-        self.image = animation[int(self.frame_index)]
+        image = animation[int(self.frame_index)]
+        if self.facing_right:
+            self.image = image
+        else:
+            flipped_image = pygame.transform.flip(image, True, False)
+            self.image = flipped_image
 
     def get_input(self):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT]:
+            self.facing_right = False
             self.direction.x = -1
         elif keys[pygame.K_RIGHT]:
+            self.facing_right = True
             self.direction.x = 1
         else:
             self.direction.x = 0
 
         if keys[pygame.K_SPACE]:
             self.jump()
+
+    # Player movement Type ...
+    def get_status(self):
+        if self.direction.y < 0:
+            self.status = "jump"
+        elif self.direction.y > 1:
+            self.status = "fall"
+        else:
+            if self.direction.x != 0:
+                self.status = "run"
+            else:
+                self.status = "idle"
 
     def apply_gravity(self):
         self.direction.y += self.gravity
@@ -57,4 +79,5 @@ class Player(pygame.sprite.Sprite):
 
     def update(self):
         self.get_input()
+        self.get_status()
         self.animate_character()
