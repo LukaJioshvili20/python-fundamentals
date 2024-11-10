2189a76cc4fcb0cda8905512392b0855f3fa8ee2
LukaJioshvili <luka.jioshvili20@gmail.com>
2024-11-10 13:52:06 +0100

    feat: testing lazyvim conf

---
+++ testing-nvim/main.py
@@ -0,0 +1,18 @@
+def calculate_user_bmi(mass: float, height: float) -> float:
+    """Calculates and returns the BMI."""
+    bmi = mass / (height * height)
+    return bmi
+
+
+def ask_for_input() -> tuple[float, float]:
+    """Prompts the user for mass and height."""
+    user_mass = float(input("What is your mass in kilograms? :"))
+    user_height = float(input("What is your height in kilograms? :"))
+    return user_mass, user_height
+
+
+if __name__ == "__main__":
+    print("Starting computting.")
+    user_mass, user_height = ask_for_input()
+    total_bmi = calculate_user_bmi(user_mass, user_height)
+    print(f"Your BMi is : {total_bmi:.2f}")
